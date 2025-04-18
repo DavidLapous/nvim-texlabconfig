f93210abb63302a9a5169d96fb72f179bd3ef369
David Loiseaux <david.loiseaux@inria.fr>
2025-04-18 18:09:25 +0200

    remove log

--- lua/texlabconfig/fn.lua
+++ lua/texlabconfig/fn.lua
@@ -7,7 +7,6 @@ local utils = require('texlabconfig.utils')
 local M = {}
 
 function M:inverse_search(filename, line, column)
-    print(column)
     column = column or 0
     local config = require('texlabconfig.config').options
 
